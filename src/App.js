1b227744879c0825d270d7d45074fd477bd2be0e
sergiogval <sergioval@gmail.com>
2022-01-15 19:18:32 -0600

    Add modules

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-rafce
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components'
+
+
+
+const app = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default app
