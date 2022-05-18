b8f3ff9db788ec032ab4b62f2f89c7ce83b287cd
Sergio Valadez <sergioval@gmail.com>
2022-05-18 15:27:30 -0600

    Revert "Mergin fist delivery"

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -4,13 +4,7 @@ import './blog.css'
 const Blog = () => {
   return (
     <div>
-      <h1>BLOG</h1>
-      <p className='text-center'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi deserunt doloremque eveniet fuga omnis pariatur, praesentium voluptatem voluptatum? Animi aperiam aspernatur dolorum hic inventore natus optio quia tenetur veniam, voluptate.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, animi debitis dolorum earum eius enim, eveniet incidunt nesciunt, nobis nostrum praesentium quam. Architecto deleniti dolorem eveniet minus odit, vitae voluptates!
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis distinctio dolore eum maxime minima molestiae nobis officia possimus! Esse exercitationem expedita facere illum ipsum obcaecati quam quos sint, temporibus voluptates?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur consequuntur cupiditate ducimus expedita incidunt iste laboriosam laborum molestiae nemo nihil, odit placeat porro quam rerum tempore totam. Magni, sequi.
-      </p>
+      Blog
     </div>
   )
 }
