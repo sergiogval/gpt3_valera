8df466b40d2f9b77ccde5ea0cd8c96a445857eb0
Sergio Valadez <sergioval@gmail.com>
2022-05-19 19:57:12 -0500

    Add all components and containers.

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import './brand.css'
+import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+import './brand.css';
 
-const Brand = () => {
-  return (
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
     <div>
-      Brand
+      <img src={google} alt="google" />
     </div>
-  )
-}
+    <div>
+      <img src={slack} alt="slack" />
+    </div>
+    <div>
+      <img src={atlassian} alt="atlassian" />
+    </div>
+    <div>
+      <img src={dropbox} alt="dropbox" />
+    </div>
+    <div>
+      <img src={shopify} alt="shopify" />
+    </div>
+  </div>
+);
 
-export default Brand
+export default Brand;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -15,13 +15,13 @@ const Header = () => (
       </div>
 
       <div className="gpt3__header-content__people">
-        <img src={people} />
+        <img src={people} alt="people" />
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
 
     <div className="gpt3__header-image">
-      <img src={ai} />
+      <img src={ai} alt="ai" />
     </div>
   </div>
 );
